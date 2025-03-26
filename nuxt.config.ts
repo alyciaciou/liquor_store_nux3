baa164b229b9a6b35c7cd0f16a64c799d77ca445
“jiafen” <qw159321@wisenvr.com>
2025-03-26 15:17:28 +0800

    新增yaml檔

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  runtimeConfig:{
-    public:{
-      baseUrl:'https://vue3-course-api.hexschool.io/v2',
-      appBase: '/liquor_store/'
-    }
-  },
-  app:{
-    baseURL:'/liquor_store/'
-  },
-  modules: ['@nuxtjs/tailwindcss','@pinia/nuxt'],
-  components:{
-    dirs:[
-      {
-        path:'~/components/global',
-        global:true
-      }
-    ]
-  }
-})
+    compatibilityDate: "2024-11-01",
+    devtools: { enabled: true },
+    ssr: false,
+    runtimeConfig: {
+        public: {
+            baseUrl: "https://vue3-course-api.hexschool.io/v2",
+            appBase: "/liquor_store/",
+        },
+    },
+    app: {
+        baseURL: "/liquor_store/",
+    },
+    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+    components: {
+        dirs: [
+            {
+                path: "~/components/global",
+                global: true,
+            },
+        ],
+    },
+});
